0929c64ddd1c7c976e564d25136ebe77e2af0f30
Marco Oliani <marco.oliani@studenti.univr.it>
2022-12-05 12:14:02 +0100

    Update ActuatorsBehaviour

--- PLC-RE/daikon/test_infoact.py
+++ PLC-RE/daikon/test_infoact.py
@@ -1,51 +1,114 @@
-import findActuators as fA
+#!/usr/bin/env python3
+
 import pandas as pd
-import numpy as np
 import os
-import configparser
 import argparse
+import configparser
+import subprocess
+import re
+import findActuators as fA
 
 
 class ActuatorsBehaviour:
     def __init__(self):
-        fa = fA.FindActuators()
-
         self.config = configparser.ConfigParser()
         self.config.read('../config.ini')
 
         parser = argparse.ArgumentParser()
         parser.add_argument('-f', "--filename", type=str, help="name of the input dataset file (CSV format)")
+        parser.add_argument('-l', "--actuatorlist", default=False, help="print actuators list")
+        parser.add_argument('-a', "--actuator", type=str, required=True, help="actuator's name")
+        parser.add_argument('-s', "--sensor", type=str, required=True, help="sensor's name")
         self.args = parser.parse_args()
 
         self.dataset = self.config['DEFAULTS']['dataset_file']
+        self.actuators = dict()
+        self.setpoints = list()
 
-        self.start_dir = os.getcwd()
+        self.actuator = None
+        self.sensor = None
 
-        if os.chdir('Daikon_Invariants/'):
+    def check_args(self):
+        if self.args.filename is not None:
+            if self.args.filename.split('.')[-1] != "csv":
+                print("Invalid file format (must be .csv). Aborting")
+                exit(1)
+            self.dataset = self.args.filename
+
+        self.actuator = self.args.actuator
+        self.sensor = self.args.sensor
+
+    def call_daikon(self):
+        dataset_name = self.dataset.split('/')[-1].split('.')[0]
+
+        # print(f"Generating {dataset_name}.decls and {dataset_name}.dtrace files ...")
+        if subprocess.call(f'perl $DAIKONDIR/scripts/convertcsv.pl {self.dataset}', shell=True):
             print("Error generating invariants. Aborting.")
             exit(1)
 
-        out = fa.call_daikon()
-        fa.parse_output(out)
-        fa.find_setpoints(out)
+        # print("Generating invariants with no conditions ...")
+        output = subprocess.check_output(
+            f'java -cp $DAIKONDIR/daikon.jar daikon.Daikon --nohierarchy {dataset_name}.decls {dataset_name}.dtrace ',
+            shell=True)
 
-        self.actuators = fa.actuators
-        self.setpoints = fa.setpoints
+        output = output.decode("utf-8")
+        output = re.sub('[=]{6,}', '', output)
+        output = output.split('\n')[6:-2]
 
-    @staticmethod
-    def open_dataframe(file, actuator, sensor):
+        return output
+
+    def find_actuators(self, output):
+        for inv in output:
+            if 'one of' in inv and self.config['DATASET']['prev_cols_prefix'] not in inv and \
+                    self.config['DATASET']['slope_cols_prefix'] not in inv:
+                a, b = inv.split(' one of ')
+                a.replace('(', '').replace(')', '')
+                b = [float(i) for i in b.replace('{ ', '').replace(' }', '').replace(',', '').split(' ')]
+
+                self.actuators[a] = b
+
+                equals = self.find_other_actuators(a, output)
+                for act in equals:
+                    self.actuators[act] = b
+
+    def find_other_actuators(self, actuator, daikon_output):
+        equals = list()
+        for inv in daikon_output:
+            if ' == ' in inv and \
+                    actuator in inv and \
+                    self.config['DATASET']['prev_cols_prefix'] not in inv \
+                    and '%' not in inv:
+                equals = inv.split(' == ')
+                equals.remove(actuator)
+
+        return equals
+
+    def open_dataframe(self, file, actuator, sensor):
         df = pd.read_csv(file, usecols=[actuator, 'prev_' + actuator, sensor])
         # print(df.keys())
         # print(df[(df[actuator] == 2) & (df['prev_'+actuator] != 2)].count())
-        print(df[df.eval(f'{actuator} == 0 and prev_{actuator} != 0')])
+
+        for key, val in self.actuators.items():
+            if key == actuator:
+                for v in val:
+                    print(df[df.eval(f'{actuator} == {v} and {self.config["DATASET"]["prev_cols_prefix"]}{actuator} != {v}')])
+                    print()
 
 
 def main():
     ab = ActuatorsBehaviour()
+    ab.check_args()
+    start_dir = os.getcwd()
+
+    if os.chdir('Daikon_Invariants/'):
+        print("Error generating invariants. Aborting.")
+        exit(1)
     # print(ab.actuators)
     # print(ab.setpoints)
+    output = ab.call_daikon()
+    ab.find_actuators(output)
 
-    ab.open_dataframe(ab.dataset, 'MV201', 'LIT301')
+    ab.open_dataframe(ab.dataset, ab.actuator, ab.sensor)
 
 
 if __name__ == '__main__':
