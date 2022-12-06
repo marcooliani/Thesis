44648fa6eeb1fd4c25e5de3922e0d1005f70b42f
Marco Oliani <marco.oliani@studenti.univr.it>
2022-12-06 16:37:23 +0100

    Update ActuatorsBehaviour

--- PLC-RE/daikon/test_infoact.py
+++ PLC-RE/daikon/test_infoact.py
@@ -6,7 +6,6 @@ import argparse
 import configparser
 import subprocess
 import re
-import findActuators as fA
 
 
 class ActuatorsBehaviour:
@@ -16,7 +15,7 @@ class ActuatorsBehaviour:
 
         parser = argparse.ArgumentParser()
         parser.add_argument('-f', "--filename", type=str, help="name of the input dataset file (CSV format)")
-        parser.add_argument('-l', "--actuatorlist", default=False, help="print actuators list")
+        parser.add_argument('-l', "--actuatorslist", default=False, help="print actuators list")
         parser.add_argument('-a', "--actuator", type=str, required=True, help="actuator's name")
         parser.add_argument('-s', "--sensor", type=str, required=True, help="sensor's name")
         self.args = parser.parse_args()
@@ -57,7 +56,20 @@ class ActuatorsBehaviour:
 
         return output
 
-    def find_actuators(self, output):
+    def find_single_actuator(self, output):
+        for inv in output:
+            if 'one of' in inv and self.config['DATASET']['prev_cols_prefix'] not in inv and \
+                    self.config['DATASET']['slope_cols_prefix'] not in inv and \
+                    self.actuator in inv:
+                a, b = inv.split(' one of ')
+                a.replace('(', '').replace(')', '')
+                b = [float(i) for i in b.replace('{ ', '').replace(' }', '').replace(',', '').split(' ')]
+
+                self.actuators[a] = b
+
+                return
+
+    def find_actuators_list(self, output):
         for inv in output:
             if 'one of' in inv and self.config['DATASET']['prev_cols_prefix'] not in inv and \
                     self.config['DATASET']['slope_cols_prefix'] not in inv:
@@ -67,11 +79,11 @@ class ActuatorsBehaviour:
 
                 self.actuators[a] = b
 
-                equals = self.find_other_actuators(a, output)
+                equals = self.__find_other_actuators(a, output)
                 for act in equals:
                     self.actuators[act] = b
 
-    def find_other_actuators(self, actuator, daikon_output):
+    def __find_other_actuators(self, actuator, daikon_output):
         equals = list()
         for inv in daikon_output:
             if ' == ' in inv and \
@@ -83,24 +95,33 @@ class ActuatorsBehaviour:
 
         return equals
 
-    @staticmethod
-    def __open_dataframe(file, actuator, sensor):
-        df = pd.read_csv(file, usecols=[actuator, 'prev_' + actuator, sensor])
-        # print(df.keys())
-        # print(df[(df[actuator] == 2) & (df['prev_'+actuator] != 2)].count())
-        return df
-
     def actuator_status_change(self, file, actuator, sensor):
-        df = self.__open_dataframe(file, actuator, sensor)
-        for key, val in self.actuators.items():
-            if key == actuator:
-                for v in val:
-                    print(df[df.eval(f'{actuator} == {v} and {self.config["DATASET"]["prev_cols_prefix"]}{actuator} != {v}')])
-                    print()
+        df = pd.read_csv(file, usecols=[actuator, self.config["DATASET"]["prev_cols_prefix"] + actuator, sensor])
 
-    def actuator_status_period(self, file, actuator, sensor):
-        df = self.__open_dataframe(file, actuator, sensor)
-        print(df[(df[actuator] == 2) & (df['prev_'+actuator] == 2)].count())
+        for v in self.actuators[actuator]:
+            print(df[df.eval(f'{actuator} == {v} and {self.config["DATASET"]["prev_cols_prefix"]}{actuator} != {v}')])
+            print()
+
+    def actuator_status_period(self, file, actuator):
+        df = pd.read_csv(file, encoding='utf-8', usecols=[actuator])
+        vals_act = [x for x in df[actuator]]
+
+        for v in self.actuators[actuator]:
+            period = list()
+            count_vals = 0
+            for i in range(1, len(vals_act)):
+                if vals_act[i] == v and vals_act[i-1] != v:
+                    count_vals += 1
+                elif vals_act[i] == v and vals_act[i-1] == v:
+                    count_vals += 1
+                elif vals_act[i] != v and vals_act[i-1] == v:
+                    count_vals += 1
+                    period.append(count_vals)
+                    count_vals = 0
+
+            print(f'{actuator} == {v}')
+            print('  '.join(map(str, period)))
+            print()
 
 
 def main():
@@ -114,10 +135,10 @@ def main():
     # print(ab.actuators)
     # print(ab.setpoints)
     output = ab.call_daikon()
-    ab.find_actuators(output)
+    ab.find_single_actuator(output)
 
     ab.actuator_status_change(ab.dataset, ab.actuator, ab.sensor)
-    ab.actuator_status_period(ab.dataset, ab.actuator, ab.sensor)
+    ab.actuator_status_period(ab.dataset, ab.actuator)
 
 
 if __name__ == '__main__':
