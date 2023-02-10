e35a2862011313f9cc39e779374519fbc8626ed3
Marco Oliani <marco.oliani@studenti.univr.it>
2023-02-10 12:26:59 +0100

    Update csv extractor

--- PLC-RE/network-analysis/swat_csv_extractor.py
+++ PLC-RE/network-analysis/swat_csv_extractor.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 
 import pandas as pd
 import csv
@@ -15,17 +16,50 @@ class SwatCSVExtractor:
         self.config.read('../config.ini')
 
         parser = argparse.ArgumentParser()
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument('-f', "--file", type=str, help="single pcap file to include (with path")
+        group.add_argument('-m', "--mergefiles", nargs='+', default=[], help="multiple pcap files to include (w/o path)")
+        group.add_argument('-d', "--mergedir", type=str, help="directory containing pcap files to merge")
+        parser.add_argument('-t', "--timerange", nargs=2, default=[], help="time range selection (format YYYY-MM-DD HH:MM:SS")
         self.args = parser.parse_args()
 
         self.df = None
+        self.csv_file = self.args.file
+        self.csv_multiple = self.args.mergefiles
+        self.csv_dir = None
+        self.csv_timerange = self.args.timerange
 
     def check_args(self):
-        pass
+        if self.args.mergedir:
+            self.csv_dir = self.args.mergedir
 
-    def import_csv(self, file):
-        self.df = pd.read_csv('~/UniVr/Tesi/datasets_SWaT/Altro/nwscan.csv',
-                              usecols=['date', 'time', 'src', 'dst', 'appi_name', 'Modbus_Function_Description',
-                                       'Modbus_Transaction_ID', 'SCADA_Tag', 'Modbus_Value', 'Tag'])
+    def import_csv(self):
+        if self.csv_file:
+            '''
+            self.df = pd.read_csv('~/UniVr/Tesi/datasets_SWaT/Altro/nwscan.csv',
+                                  usecols=['date', 'time', 'src', 'dst', 'appi_name', 'Modbus_Function_Description',
+                                           'Modbus_Transaction_ID', 'SCADA_Tag', 'Modbus_Value', 'Tag'])
+            '''
+            self.df = pd.read_csv(self.csv_file,
+                                  usecols=['date', 'time', 'src', 'dst', 'appi_name', 'Modbus_Function_Description',
+                                           'Modbus_Transaction_ID', 'SCADA_Tag', 'Modbus_Value', 'Tag'])
+
+        if self.csv_multiple:
+            self.df = pd.concat(map(lambda file:
+                                    pd.read_csv(file,
+                                                usecols=['date', 'time', 'src', 'dst', 'appi_name',
+                                                         'Modbus_Function_Description', 'Modbus_Transaction_ID',
+                                                         'SCADA_Tag', 'Modbus_Value', 'Tag'], header=0),
+                                    self.csv_multiple), ignore_index=True)
+
+        if self.csv_dir:
+            print(self.csv_dir)
+            self.df = pd.concat(map(lambda file:
+                                    pd.read_csv(file,
+                                                usecols=['date', 'time', 'src', 'dst', 'appi_name',
+                                                         'Modbus_Function_Description', 'Modbus_Transaction_ID',
+                                                         'SCADA_Tag', 'Modbus_Value', 'Tag'], header=0),
+                                    glob.glob(os.path.join(self.csv_dir, '*.csv'))), ignore_index=True)
 
         self.df[self.config['DATASET']['timestamp_col']] = \
             pd.to_datetime(self.df['date']).dt.strftime('%Y-%m-%d') + " " + \
@@ -51,7 +85,8 @@ class SwatCSVExtractor:
 
 def main():
     sce = SwatCSVExtractor()
-    sce.import_csv('bla')
+    sce.check_args()
+    sce.import_csv()
 
 
 if __name__ == '__main__':
